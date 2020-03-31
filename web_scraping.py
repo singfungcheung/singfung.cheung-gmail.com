33ac0853c402a0d91199a155ddf95b16d1a3f64a
Sing Fung Cheung <singfung.cheung@gmail.com>
2020-03-31 09:25:52 -0400

    first edit

---
+++ web_scraping.py
@@ -0,0 +1,10 @@
+Initialized empty Git repository in C:/Users/Sing Fung/Desktop/Developer/Python/web_scraping/.git/
+On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	web_scraping.py
+
+nothing added to commit but untracked files present (use "git add" to track)
